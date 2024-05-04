daea867c80a600ae82f58a4ccd7fbb08f152e18c
Jakub Zedník <91053389+kuboi1@users.noreply.github.com>
2024-05-04 18:21:43 +0200

    add controls to ascii video player

--- ascii_video_player.py
+++ ascii_video_player.py
@@ -2,6 +2,7 @@ import json
 import pickle
 import os
 import time
+import keyboard
 
 class AsciiVideoPlayer:
     def __init__(self, default_frame_rate: int = 24) -> None:
@@ -9,6 +10,11 @@ class AsciiVideoPlayer:
 
         self._frames = {}
         self._frame_rows = 0
+        self._frame_cols = 0
+        self._first_frame = 0
+        self._current_frame = 0
+
+        self._paused = False
 
     def play(self, path: str, clear_before: bool = False) -> None:
         print('Loading video...')
@@ -23,26 +29,34 @@ class AsciiVideoPlayer:
         frame_keys = list(input_frames.keys())
         frame_keys.sort(key=int)
         self._frames = {i: input_frames[i] for i in frame_keys}
+
+        # Set frame dimensions
         self._frame_rows = len(list(self._frames.values())[0])
+        self._frame_cols = len(list(self._frames.values())[0][0])
+
+        # Set first frame key
+        self._first_frame = self._current_frame = int(list(self._frames.keys())[0])
 
         if clear_before:
-            self._prep_next_frame()
+            self._clear_console()
         
-        passed_frames = 0
-
-        for frame_key in self._frames:
+        while self._has_next_frame():
             frame_start = time.perf_counter()
 
-            self._display_frame(self._frames[frame_key])
+            # Display the ascii frame
+            self._display_frame(self._frames[str(self._current_frame)])
+
+            # Print a seperator
+            self._print_seperator()
+
+            # Print player controls
+            self._print_controls()
+
+            # Check for input and handle it
+            self._handle_user_input()
 
             # Prepare next frame by moving the cursor to the start
             self._prep_next_frame()
-
-            passed_frames += 1
-
-            # Clear console every 5 seconds
-            if (passed_frames % (frame_rate * 5)) == 0:
-                self._clear_console()
 
             # Limit frames per second
             time.sleep(max(0, (1.0 / frame_rate) - (time.perf_counter() - frame_start)))
@@ -71,12 +85,72 @@ class AsciiVideoPlayer:
 
         return {}
 
+    def _handle_user_input(self) -> None:
+        if self._paused:
+            # UNPAUSE
+            if keyboard.is_pressed('w'):
+                if self._paused:
+                    self._paused = False
+        else:
+            # PAUSE
+            if keyboard.is_pressed('q'):
+                self._paused = True
+                # Clear the controls line
+                print(f'\033[1A\033[2K', end='')
+                print(''.join([' ' for _ in range(self._frame_cols)]))
+                print(f'\033[1A\033[2K', end='')
+                print('| PAUSED: Press f3 to unpause |')
+            
+            # REWIND
+            if keyboard.is_pressed('e'):
+                if self._current_frame - 5 >= self._first_frame:
+                    self._current_frame -= 5
+            
+            # FAST FORWARD
+            if keyboard.is_pressed('r'):
+                self._current_frame += 4
+
+        # CLEAR ARTIFACTS
+        if keyboard.is_pressed('t'):
+            self._clear_console()
+        
+        # STOP AND TURN OFF
+        if keyboard.is_pressed('z') or keyboard.is_pressed('y'):
+            self._clear_console()
+            self._playing = False
+            print('Video stopped by controls')
+            exit(0)
+    
+    def _print_seperator(self) -> None:
+        if self._paused:
+            return
+        
+        print(''.join(['-' for _ in range(self._frame_cols)]))
+
+    def _print_controls(self) -> None:
+        if self._paused:
+            return
+        
+        print('CONTROLS: | q - Pause | e - Rewind | r - Fast forward | t - Clear artifacts | y - Stop |')
+
     def _display_frame(self, frame_data: list) -> None:
+        if self._paused:
+            return
+        
         print('\n'.join(frame_data))
 
     def _prep_next_frame(self) -> None:
-        # Move cursor up by the number of rows in one frame
-        print(f'\033[{self._frame_rows}A\033[2K', end='')
+        if self._paused:
+            return
+        
+        self._current_frame += 1
+        
+        # Move cursor up by the number of rows in one frame + 2 rows for controls
+        move_lines = self._frame_rows + 2
+        print(f'\033[{move_lines}A\033[2K', end='')
+    
+    def _has_next_frame(self) -> bool:
+        return str(self._current_frame + 1) in self._frames
     
     def _clear_console(self) -> None:
         os.system('cls')
